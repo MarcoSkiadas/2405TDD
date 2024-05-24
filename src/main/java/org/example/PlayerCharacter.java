20b19f020e3a8f07b6ebe527ea2bef77e8a7860b
MarcoSkiadas <Skiadas2@hotmail.com>
2024-05-24 15:57:45 +0200

    add move and BeforeEach to test

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -3,8 +3,10 @@ package org.example;
 public class PlayerCharacter {
     public static void main(String[] args) {
 
-    }
 
+    }
+    static int Y = 0;
+    static int X = 0;
     public static int getX(int a) {
         return a=0;
 
@@ -12,5 +14,20 @@ public class PlayerCharacter {
     public static int getY(int a) {
         return a=0;
 
+    }
+    public static int move(String a) {
+        if(a.equals("w")) {
+            return Y=Y+1;
+        }
+        else if(a.equals("s")) {
+            return Y=Y-1;
+        }
+        else if(a.equals("d")) {
+            return X=X+1;
+        }
+        else if(a.equals("a")) {
+            return X=X-1;
+        }
+        return 0;
     }
 }
--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlayerCharacterTest {
 
+    @BeforeEach
+    void setUp() {
+        // Diese Methode wird vor jedem Testfall ausgeführt
+        PlayerCharacter.X = 0;
+        PlayerCharacter.Y = 0;
+    }
     @Test
     void getX_postionShouldBe0_whenStart5() {
         //Given
@@ -21,6 +28,42 @@ class PlayerCharacterTest {
         int expected = 0;
         //when
         int actual = PlayerCharacter.getY(5);
+        //then
+        assertEquals(expected, actual);
+    }
+    @Test
+    void move_shouldReturnYOneMore_whenPressW() {
+        //Given
+        int expected = 1;
+        //when
+        int actual = PlayerCharacter.move("w");
+        //then
+        assertEquals(expected, actual);
+    }
+    @Test
+    void move_shouldReturnYOneLess_whenPressS() {
+        //Given
+        int expected = -1;
+        //when
+        int actual = PlayerCharacter.move("s");
+        //then
+        assertEquals(expected, actual);
+    }
+    @Test
+    void move_shouldReturnXOneLess_whenPressA() {
+        //Given
+        int expected = -1;
+        //when
+        int actual = PlayerCharacter.move("a");
+        //then
+        assertEquals(expected, actual);
+    }
+    @Test
+    void move_shouldReturnXOneMore_whenPressD() {
+        //Given
+        int expected = 1;
+        //when
+        int actual = PlayerCharacter.move("d");
         //then
         assertEquals(expected, actual);
     }
